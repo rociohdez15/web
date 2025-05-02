aaac62a702924a3145663429ac479624e2a309f2
rociohdez15 <rociohdez2706@hotmail.com>
2025-05-02 16:08:55 +0200

    Creación del endpoint para eliminar administradores.

--- backend/src/main/java/com/web/backend/controller/AdminController.java
+++ backend/src/main/java/com/web/backend/controller/AdminController.java
@@ -6,13 +6,16 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.web.backend.model.Evento;
 import com.web.backend.model.User;
+import com.web.backend.repository.UserRepository;
 import com.web.backend.service.AdminsService;
 
 @RestController
@@ -22,6 +25,8 @@ public class AdminController {
     @Autowired
     private AdminsService adminsService;
 
+    @Autowired
+    private UserRepository usuarioRepository;
 
     @GetMapping("/obtener-admins")
     public List<User> obtenerAdmins() {
@@ -30,13 +35,25 @@ public class AdminController {
 
     @GetMapping("/buscar")
     public ResponseEntity<?> buscarUsuario(@RequestParam String input) {
-    Optional<User> usuario = adminsService.buscarPorIdOEmail(input);
+        Optional<User> usuario = adminsService.buscarPorIdOEmail(input);
 
-    if (usuario.isPresent()) {
-        return ResponseEntity.ok(usuario.get());
-    } else {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No se encontraron resultados.");
+        if (usuario.isPresent()) {
+            return ResponseEntity.ok(usuario.get());
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No se encontraron resultados.");
+        }
     }
-}
+
+    @DeleteMapping("/eliminar/{id}")
+    public ResponseEntity<?> eliminarAdmin(@PathVariable Long id) {
+        try {
+            usuarioRepository.deleteById(id);
+            return ResponseEntity.ok().body("Administrador eliminado correctamente");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                                .body("Error al eliminar el administrador");
+        }
+    }
+
 
 }
