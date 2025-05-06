031c2bc15acdbf67ea1807ffc640263bdc898498
rociohdez15 <rociohdez2706@hotmail.com>
2025-05-06 17:56:50 +0200

    Creación del endpoint para el buscador del listado de eventos.

--- backend/src/main/java/com/web/backend/repository/EventoRepository.java
+++ backend/src/main/java/com/web/backend/repository/EventoRepository.java
@@ -1,11 +1,15 @@
 package com.web.backend.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.web.backend.model.Evento;
+import com.web.backend.model.User;
 
 @Repository
 public interface EventoRepository extends JpaRepository<Evento, Long>{
-
+    Optional<Evento> findById(Long id);
+    Optional<Evento> findByNombre(String nombre);
 }
