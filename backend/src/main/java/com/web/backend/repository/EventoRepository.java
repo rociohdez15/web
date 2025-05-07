05d745a46569681af2da48e47bfb535799499949
rociohdez15 <rociohdez2706@hotmail.com>
2025-05-07 16:33:41 +0200

    Creación del backend para crear eventos.

--- backend/src/main/java/com/web/backend/repository/EventoRepository.java
+++ backend/src/main/java/com/web/backend/repository/EventoRepository.java
@@ -1,5 +1,7 @@
 package com.web.backend.repository;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,4 +15,9 @@ import com.web.backend.model.User;
 public interface EventoRepository extends JpaRepository<Evento, Long>{
     Optional<Evento> findById(Long id);
     List<Evento> findByNombre(String nombre);
+       // Verifica si ya existe un evento con mismo nombre, fecha y hora
+    Optional<Evento> findByNombreAndFechaAndHora(String nombre, LocalDate fecha, LocalTime hora);
+
+    // Verifica si ya existe un evento (cualquiera) en esa fecha y hora
+    Optional<Evento> findByFechaAndHora(LocalDate fecha, LocalTime hora);
 }
